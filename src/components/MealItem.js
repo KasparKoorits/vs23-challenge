ec1fb2cc213701ec2db9da40abacb00d33fb74ab
KasparKoorits <kaspar.koorits@voco.ee>
2025-01-13 14:33:07 +0200

    Solved exercise 5

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,13 +1,18 @@
 const MealItem = (props) => {
   const imagePath = require(`../assets/${props.meal.image}`);
 
+  const formattedPrice = new Intl.NumberFormat("de-DE", {
+    style: "currency",
+    currency: "EUR",
+  }).format(props.meal.price);
+
   return (
     <li className="meal-item">
       <article>
         <img src={imagePath} alt={props.meal.name} />
         <div className="meal-item-description">
           <h3>{props.meal.name}</h3>
-          <p className="meal-item-price">{props.meal.price}</p>
+          <p className="meal-item-price">{formattedPrice}</p>
           <p>{props.meal.description}</p>
         </div>
         <div className="meal-item-actions">
