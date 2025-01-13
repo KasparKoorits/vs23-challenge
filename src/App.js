c9dc411dab675e4c8fd704f970a4e6e73c819a06
KasparKoorits <kaspar.koorits@voco.ee>
2025-01-13 13:26:50 +0200

    Solved exercise 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Header from "./components/Header";
+import Meals from "./components/Meals";
+
 const App = () => {
- return (
-    <>
-     <h1>Food Order App</h1>
-    </>
+  return (
+    <div>
+      <Header />
+      <Meals />
+    </div>
   );
-}
+};
 
 export default App;
