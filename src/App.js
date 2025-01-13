da700f1fd1c0a1f764ac2ef7dff331ba10d924bb
KasparKoorits <kaspar.koorits@voco.ee>
2025-01-13 13:51:26 +0200

    Solved exercise 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import Header from "./components/Header";
-import Meals from "./components/Meals";
+import Meals from "./components/Meals"; // pole vaja veel aga las olla
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Meals />
     </div>
   );
 };
